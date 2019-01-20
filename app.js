e877a8a2b26df49eff6e4c6310a98cce6c79ef4b
le thanh trung <lethanhtrung5121992@gmail.com>
2019-01-20 17:49:24 +0700

    cat nhat cross origin

--- app.js
+++ app.js
@@ -85,6 +85,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
+app.use(function(req,res,next){
+	res.header('Access-Control-Allow-Origin','*');
+	res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers','Content-Type');
+	next();
+})
+
+
 /************* validate ************/
 app.use(validator({
 	customValidators:{
