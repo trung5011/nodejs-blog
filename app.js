4db0d78c6acd9b0f1bc5f404603eff2689edda9b
le thanh trung <lethanhtrung5121992@gmail.com>
2019-02-21 15:31:44 +0700

    cat naht heeader

--- app.js
+++ app.js
@@ -88,8 +88,15 @@ app.use(passport.session());
 
 
 app.use(function(req,res,next){
-	res.header('Access-Control-Allow-Origin','*');
-	res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+	  "Access-Control-Allow-Headers",
+	  "Origin, X-Requested-With, Content-Type, Accept"
+	);
+	res.setHeader(
+	  "Access-Control-Allow-Methods",
+	  "GET, POST, PATCH, DELETE, OPTIONS"
+	);
 	next();
 })
 
