7923569dd405c511edbc6e2bddb12a8874f81869
le thanh trung <lethanhtrung5121992@gmail.com>
2019-02-23 09:26:33 +0700

    cat nhat header

--- app.js
+++ app.js
@@ -88,16 +88,17 @@ app.use(passport.session());
 
 
 app.use(function(req,res,next){
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader(
-	  "Access-Control-Allow-Headers",
-	  "Origin, X-Requested-With, Content-Type, Accept"
-	);
-	res.setHeader(
-	  "Access-Control-Allow-Methods",
-	  "GET, POST, PATCH, DELETE, OPTIONS"
-	);
-	next();
+ res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
 })
 
 
